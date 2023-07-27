8dfee0cca1f8714172e6110b85d88e5856c8092f
lajtosrob <lajtosrobert@freemail.hu>
2023-07-27 11:02:52 +0200

    Adding forms and Fetching the API into Django

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 
+# QCoyHaSfUQnLKdVzEiFNbQ==slDySxHaLKhWzTHx
 # Create your views here.
 def home(request):
-    
-    return render(request, 'home.html')
+    import json
+    import requests
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get (api_url + query, headers = {'X-Api-Key': 'QCoyHaSfUQnLKdVzEiFNbQ==slDySxHaLKhWzTHx'})
+        try: 
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! The was an error"
+            print(e)
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'query': 'Enter a valid query'})
+
+
+
+
+
+
+        """ query = '1lb brisket and fries'
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+        response = requests.get(api_url, headers={'X-Api-Key': 'QCoyHaSfUQnLKdVzEiFNbQ==slDySxHaLKhWzTHx'})
+        if response.status_code == requests.codes.ok:
+            print(response.text)
+        else:
+            print("Error:", response.status_code, response.text)
+        return render(request, 'home.html') """
